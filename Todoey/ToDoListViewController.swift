c276000b245b886483facb20e3037f9cac9835be
lordchingiss <jackli2014@gmail.com>
2019-08-22 14:32:59 -0700

    Added TableView Datasource and Delegate methods

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ToDoListViewController: UITableViewController {
+    let toDos = ["Take a shit", "Buy condoms", "Smoke Weed"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        //let cell = tableView.cellForRow(at: indexPath)
+        cell.textLabel?.text = toDos[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+    
+    // MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            print("Selected cell containing: \(toDos[indexPath.row])")
+            tableView.deselectRow(at: indexPath, animated: true)
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            }
+            else {
+                cell.accessoryType = .none
+            }
+        }
+    }
 }
 
